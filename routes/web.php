84361bec99b429a089455ff9ae6f5f66c030dbd9
alvian <alvian.ardhiansyah@gmail.com>
2020-01-15 23:12:58 +0700

    revisi

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,21 @@ Route::get('/', function () {
     return view('welcome');
     //master- nama file
 });
+Route::get('/login','AuthController@login')->name('login.index');
+Route::post('/login','AuthController@postLogin')->name('login');
+Route::get('/register','AuthController@register')->name('register.index');
+Route::post('/register','AuthController@postRegister')->name('register');
 
-Route::get('/dashboard/chart-income', 'DashboardController@chartIncome');
-Route::get('/dashboard/chart-expense', 'DashboardController@chartExpense');
-Route::post('/danaimpian/fetch','HopeController@fetch');
-Route::post('/danaimpian/hitung','HopeController@hitung');
-Route::post('/danaimpian/save','HopeController@savedata');
-Route::resource('dashboard', 'DashboardController');
-Route::resource('pemasukan', 'IncomeController');
-Route::resource('pengeluaran', 'ExpenseController');
-Route::resource('danaimpian', 'HopeController');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/dashboard/chart-income', 'DashboardController@chartIncome');
+    Route::get('/dashboard/chart-expense', 'DashboardController@chartExpense');
+    Route::post('/danaimpian/fetch','HopeController@fetch');
+    Route::post('/danaimpian/hitung','HopeController@hitung');
+    Route::post('/danaimpian/save','HopeController@savedata');
+    Route::resource('dashboard', 'DashboardController');
+    Route::resource('pemasukan', 'IncomeController');
+    Route::resource('pengeluaran', 'ExpenseController');
+    Route::resource('danaimpian', 'HopeController');
+    Route::get('/logout','AuthController@logout')->name('logout');
+
+});
